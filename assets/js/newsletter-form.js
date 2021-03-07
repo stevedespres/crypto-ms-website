63cb9288db23397c4d26da7dc9bf3d3d8daf6e10
Steve Despres <steve.despres@protonmail.com>
2021-03-07 10:57:50 +0100

    Add newsletter

---
+++ assets/js/newsletter-form.js
@@ -0,0 +1,16 @@
+window.REQUIRED_CODE_ERROR_MESSAGE = 'Veuillez choisir un code pays';
+  
+window.EMAIL_INVALID_MESSAGE = window.SMS_INVALID_MESSAGE = "Les informations que vous avez fournies ne sont pas valides. Veuillez vérifier le format du champ et réessayer.";
+
+window.REQUIRED_ERROR_MESSAGE = "Vous devez renseigner ce champ. ";
+
+window.GENERIC_INVALID_MESSAGE = "Les informations que vous avez fournies ne sont pas valides. Veuillez vérifier le format du champ et réessayer.";
+
+window.translation = {
+  common: {
+    selectedList: '{quantity} liste sélectionnée',
+    selectedLists: '{quantity} listes sélectionnées'
+  }
+};
+
+var AUTOHIDE = Boolean(0);
